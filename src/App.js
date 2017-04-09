3bf6b498aba89c0b9ea3d2a597c9e3ed97be9422
BadWaka <1456683844@qq.com>
2017-04-09 17:29:22 +0800

    bak

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,9 +6,6 @@ class App extends Component {
     return (
       <div className="App">
         App
-        <div>
-          App2
-        </div>
       </div>
     );
   }
