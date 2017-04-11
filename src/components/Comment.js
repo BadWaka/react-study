499be94d62a768e0463767ff6830836f0a258fe9
BadWaka <1456683844@qq.com>
2017-04-11 19:50:12 +0800

    bak

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -8,7 +8,7 @@ export default class Comment extends Component {
 
     static propTypes = {
         comment: PropTypes.object.isRequired,
-    }
+    };
 
     constructor() {
         super();
@@ -25,6 +25,11 @@ export default class Comment extends Component {
         const comment = this.props.comment;
         console.log('comment', comment);
         const duration = (+Date.now() - comment.createdTime) / 1000;    // 经过时间
+        this.setState({
+            timeString: duration > 60
+                ? `${Math.round(duration / 60)} 分钟前`
+                : `${Math.round(Math.max(duration, 1))} 秒前`
+        });
     }
 
     render() {
@@ -34,6 +39,9 @@ export default class Comment extends Component {
                     <span>{this.props.comment.username} :</span>
                 </div>
                 <p>&nbsp;{this.props.comment.content}</p>
+                <span className="comment-createdtime">
+                    {this.state.timeString}
+                </span>
             </div>
         );
     }
