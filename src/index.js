77ac4423966c1dd2322fb040c656bd27b4a1ca83
BadWaka <1456683844@qq.com>
2017-04-18 20:26:13 +0800

    动手实现 Redux（二）：抽离 store 和监控数据变化（一半）

--- src/index.js
+++ src/index.js
@@ -39,6 +39,15 @@ function dispatch(action) {
     }
 }
 
+function createStore(state, stateChanger) {
+    const getState = () => state;
+    const dispatch = (action) => stateChanger(state, action);
+    return {
+        getState,
+        dispatch
+    }
+}
+
 renderApp(appState);
 
 dispatch({
