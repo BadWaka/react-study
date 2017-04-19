f9be2a02ad2b58007fdaed07cfd43fcc64fb4121
BadWaka <1456683844@qq.com>
2017-04-19 09:40:13 +0800

    动手实现 Redux（四）：共享结构的对象提高性能

--- src/index.js
+++ src/index.js
@@ -1,56 +1,3 @@
-const appState = {
-    title: {
-        text: 'React.js 小书',
-        color: 'red'
-    },
-    content: {
-        text: 'React.js 小书内容',
-        color: 'blue'
-    }
-};
-
-function renderApp(newAppState, oldAppState = {}) {
-    if (newAppState === oldAppState) {
-        return;
-    }
-    console.log('render app...');
-    renderTitle(newAppState.title);
-    renderContent(newAppState.content);
-}
-
-function renderTitle(newTitle, oldTitle = {}) {
-    if (newTitle === oldTitle) {
-        return;
-    }
-    console.log('render title...');
-    const titleDOM = document.getElementById('title');
-    titleDOM.innerHTML = newTitle.text;
-    titleDOM.style.color = newTitle.color;
-}
-
-function renderContent(newContent, oldContent) {
-    if (newContent === oldContent) {
-        return;
-    }
-    console.log('render content...');
-    const contentDOM = document.getElementById('content');
-    contentDOM.innerHTML = newContent.text;
-    contentDOM.style.color = newContent.color;
-}
-
-function dispatch(action) {
-    switch (action.type) {
-        case 'UPDATE_TITLE_TEXT':
-            appState.title.text = action.text;
-            break;
-        case 'UPDATE_TITLE_COLOR':
-            appState.title.color = action.color;
-            break;
-        default:
-            break;
-    }
-}
-
 function createStore(state, stateChanger) {
     // 监听函数们
     const listeners = [];
@@ -63,7 +10,7 @@ function createStore(state, stateChanger) {
     const getState = () => state;
 
     const dispatch = (action) => {
-        stateChanger(state, action);
+        state = stateChanger(state, action);
         listeners.forEach((listener) => {
             listener();
         });
@@ -76,16 +23,67 @@ function createStore(state, stateChanger) {
     }
 }
 
+function renderApp(newAppState, oldAppState = {}) {
+    if (newAppState === oldAppState) {
+        return;
+    }
+    console.log('render app...');
+    renderTitle(newAppState.title, oldAppState.title);
+    renderContent(newAppState.content, oldAppState.content);
+}
+
+function renderTitle(newTitle, oldTitle = {}) {
+    if (newTitle === oldTitle) {
+        return;
+    }
+    console.log('render title...');
+    const titleDOM = document.getElementById('title');
+    titleDOM.innerHTML = newTitle.text;
+    titleDOM.style.color = newTitle.color;
+}
+
+function renderContent(newContent, oldContent = {}) {
+    // console.log('newContent', newContent, 'oldContent', oldContent);
+    if (newContent === oldContent) {
+        return;
+    }
+    console.log('render content...');
+    const contentDOM = document.getElementById('content');
+    contentDOM.innerHTML = newContent.text;
+    contentDOM.style.color = newContent.color;
+}
+
+let appState = {
+    title: {
+        text: 'React.js 小书',
+        color: 'red'
+    },
+    content: {
+        text: 'React.js 小书内容',
+        color: 'blue'
+    }
+};
+
 function stateChanger(state, action) {
     switch (action.type) {
         case 'UPDATE_TITLE_TEXT':
-            state.title.text = action.text;
-            break;
+            return {    // 构建新的对象并且返回
+                ...state,
+                title: {
+                    ...state.title,
+                    text: action.text
+                }
+            };
         case 'UPDATE_TITLE_COLOR':
-            state.title.color = action.color;
-            break;
+            return {
+                ...state,
+                title: {
+                    ...state.title,
+                    color: action.color
+                }
+            };
         default:
-            break;
+            return state;
     }
 }
 
