a98e06e716798a3c79037fbc780ab17f5aae277c
BadWaka <1456683844@qq.com>
2017-04-23 22:35:41 +0800

    bak

--- src/components/CommentInput.js
+++ src/components/CommentInput.js
@@ -1,3 +1,6 @@
+/**
+ * Created by waka on 2017/4/10.
+ */
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
@@ -5,7 +8,7 @@ export default class CommentInput extends Component {
 
     static propTypes = {
         username: PropTypes.any,
-        onSubmit: PropTypes.func,
+        onSubmit: PropTypes.func,   // 提交事件
         onUserNameInputBlur: PropTypes.func,
     };
 
@@ -18,8 +21,93 @@ export default class CommentInput extends Component {
         this.state = {
             username: props.username,
             content: ''
-        };
+        }
     }
 
+    componentDidMount() {
+        this.textarea.focus();
+    }
 
+    handleUserNameBlur(event) {
+        if (this.props.onUserNameInputBlur) {
+            this.props.onUserNameInputBlur(event.target.value);
+        }
+    }
+
+    handleUserNameChange(event) {
+        this.setState(() => {
+            return {
+                username: event.target.value
+            }
+        });
+    }
+
+    handleContentChange(event) {
+        this.setState(() => {
+            return {
+                content: event.target.value
+            }
+        });
+    }
+
+    handleSubmit() {
+        if (this.props.onSubmit) {
+
+        }
+    }
+
+    render() {
+        return (
+            <div className="comment-input">
+                <div className="comment-field">
+                    <span className="comment-field-name">用户名：</span>
+                    <div className="comment-field-input">
+                        <input value={this.state.username}
+                            // 失去焦点
+                               onBlur={(e) => {
+                                   localStorage.setItem('username', e.target.value);
+                               }}
+                               onChange={(e) => {
+                                   this.setState({
+                                       username: e.target.value
+                                   });
+                               }}/>
+                    </div>
+                </div>
+                <div className='comment-field'>
+                    <span className='comment-field-name'>评论内容：</span>
+                    <div className='comment-field-input'>
+                        <textarea
+                            ref={(textarea) => {
+                                this.textarea = textarea;
+                            }}
+                            value={this.state.content}
+                            onChange={(e) => {
+                                this.setState({
+                                    content: e.target.value
+                                });
+                            }}/>
+                    </div>
+                </div>
+                <div className='comment-field-button'>
+                    <button
+                        onClick={() => {
+                            if (this.props.onSubmit) {
+                                const {username, content} = this.state;
+                                this.props.onSubmit({
+                                    username,
+                                    content,
+                                    createdTime: +new Date(),   // 传入创建时间
+                                });
+                            }
+                            this.setState({
+                                content: ''
+                            });
+                        }}>
+                        发布
+                    </button>
+                </div>
+            </div>
+        );
+    }
 }
